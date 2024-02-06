379c283060986c8ca7e3a3b99d162eda068f1bff
Felipe Jiménez <148123263+felipejimeenezz@users.noreply.github.com>
2024-02-06 20:18:26 +0100

    Add files via upload

---
+++ TiendaMascotasTest.java
@@ -0,0 +1,21 @@
+package Tema5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TiendaMascotasTest {
+
+    @Test
+    public void agregarMascota() {
+
+    }
+
+    @Test
+    public void venderMascota() {
+    }
+
+    @Test
+    public void mostrarInventario() {
+    }
+}
