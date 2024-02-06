c608678a1124bfcc62d5c20760e3e978e2a5cca9
Felipe Jiménez <148123263+felipejimeenezz@users.noreply.github.com>
2024-02-06 20:42:37 +0100

    subida test

--- TiendaMascotasTest.java
+++ TiendaMascotasTest.java
@@ -8,14 +8,40 @@ class TiendaMascotasTest {
 
     @Test
     public void agregarMascota() {
-
+        TiendaMascotas tienda = new TiendaMascotas();
+        tienda.agregarMascota("Alex", 9, "perro");
+            assertEquals(1, tienda.numeroMascotas);
     }
 
     @Test
     public void venderMascota() {
+        TiendaMascotas tienda = new TiendaMascotas();
+        tienda.agregarMascota("Max", 2, "gato");
+        tienda.venderMascota("Max");
+        assertEquals(0, tienda.numeroMascotas);
     }
 
     @Test
-    public void mostrarInventario() {
+    public void testVenderMascotaInexistente() {
+        TiendaMascotas tienda = new TiendaMascotas();
+        tienda.agregarMascota("Alex", 9, "perro");
+        tienda.venderMascota("Max");
+        assertEquals(1, tienda.numeroMascotas);
     }
+
+    @Test
+    public void testMostrarInventarioConMascotas() {
+        TiendaMascotas tienda = new TiendaMascotas();
+        tienda.agregarMascota("Alex", 9, "perro");
+        tienda.agregarMascota("Max", 2, "gato");
+        tienda.agregarMascota("Perry", 8, "ornitorrinco");
+        assertDoesNotThrow(() -> tienda.mostrarInventario());
+    }
+
+    @Test
+    public void testMostrarInventarioSinMascotas() {
+        TiendaMascotas tienda = new TiendaMascotas();
+        assertDoesNotThrow(() -> tienda.mostrarInventario());
+    }
+
 }
